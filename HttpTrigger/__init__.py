aa6e033623780272d017a4b8687444d630a3e6da
quinnmeagher <Quinn.Meagher@microsoft.com>
2023-03-29 11:22:59 -0400

    Added deployment processing

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -33,12 +33,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # Validation Process: Creation of EventGrid subscription will send a validationCode.
     # Subscription is validated when the validationCode is returned with a 200 status code response.
-    validationCode = any
-    try:
-        validationCode = data['validationCode']
-        return func.HttpResponse(json.dumps({"validationResponse": validationCode}))
-    except:
-        print("Already validated")
+    if 'validationCode' in data:
+        return func.HttpResponse(json.dumps({"validationResponse": data['validationCode']}), status_code=200)
 
     # Resource Management SDK Appsettings
     clientId = os.environ.get("TAG_CLIENT_ID", None)
@@ -64,23 +60,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     existingData = TagsResource
     appIdTag = any
 
-    # Query for existing tags
-    try:
-        # Filter out invalid operations
-        if 'operationName' in data:
-            if 'Microsoft.Resources/tags/write' in data['operationName']:
-                return func.HttpResponse("Ignore tag write operation", 200)
-        
-        # Get tags using resourceUri scope
-        existingData = resource_client.tags.get_at_scope(
-        data['resourceUri'])
-        appIdTag = existingData.properties.tags["appId"]
-    except:
-        print("Resource does not support tags")
-
-        # If the request does not have an appId tag or does not support tags the function returns.
-        return func.HttpResponse("Resource does not support tags or does not contain AppId tag", 200)
-
     # Instantiate CosmosDB Client using the url and access key
     cosmosClient = CosmosClient(url, key)
 
@@ -96,6 +75,77 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Create query template where $n1 is the existing AppId tag value
     queryTemplate = Template('SELECT * FROM c where c.id = "$n1"')
 
+    # Query for existing tags
+    try:
+        # Filter out invalid operations
+        if 'operationName' in data:
+            # if 'Microsoft.Resources/tags/write' in data['operationName'] or 'Microsoft.Resources/deployments/write' in data['operationName']:
+            if 'Microsoft.Resources/tags/write' in data['operationName']:
+                print("Operation was filtered for Uri: " + data['resourceUri'] + " | " + data['operationName'])
+                return func.HttpResponse("Ignore tag write operation", status_code=400)
+            
+            # Check if the operation is a deployment rather then a direct resource provider operation
+            # E.g OperationName of 'Microsoft.Resources/deployments/write' instead of 'Microsoft.StorageAccounts/write'
+            if 'Microsoft.Resources/deployments/write' in data['operationName']:
+
+                # Query for deployment information which contains the list of output resources
+                existingDeployment = resource_client.resources.get_by_id(data['resourceUri'], '2021-04-01')
+
+                # Check if the deployment has outputResources
+                if 'outputResources' in existingDeployment.properties:
+                    outputResources = existingDeployment.properties.get('outputResources')
+
+                    # Iterate through output resources, applying tags to any valid resources
+                    for resource in outputResources:
+                        existingDataForResource = TagsResource
+                        appIdTagForResource = any
+                        cosmosTagDataForResource = dict[str, any]
+                        try:
+                            # Get existing tags for resource from deployment output resource
+                            existingDataForResource = resource_client.tags.get_at_scope(resource['id'])
+
+                            # Check is AppId tag exists
+                            if 'appId' in existingDataForResource.properties.tags:
+                                appIdTagForResource = existingDataForResource.properties.tags["appId"]
+
+                            # Check if we have already applied complex tags
+                            if 'appName' in existingDataForResource.properties.tags or 'owner' in existingDataForResource.properties.tags:
+                                print("Tag updates are already in place for deployed resources")
+                                return func.HttpResponse("Tag updates are already in place for deployed resources", status_code=400)  
+
+                            # Query for complex tags from CosmosDB using the existing AppId tag
+                            for item in container.query_items(
+                                query=queryTemplate.substitute(n1 = appIdTagForResource),
+                                enable_cross_partition_query=True,
+                            ):
+                                # Copy query response into cosmosTagDataForResource dictionary
+                                cosmosTagDataForResource = item.copy()
+
+                            # Create the resource tags for the given resourceUri using the queried tags from CosmosDB
+                            resource_client.tags.create_or_update_at_scope(resource['id'], { "operation": "create", "properties": {
+                            "tags": { "appId": appIdTagForResource, "appName": cosmosTagDataForResource.get('appName'), "owner": cosmosTagDataForResource.get('owner'), "ctime": cosmosTagDataForResource.get('ctime') }}})
+                        except:
+                            print("Deployment resource does not support tags or tags were not successfully added")
+
+                    # Proper status code response prevent excessive retry
+                    return func.HttpResponse("Tag updates were processed for deployment resources", status_code=200)
+   
+            else:
+                # Get tags using resourceUri scope
+                existingData = resource_client.tags.get_at_scope(
+                data['resourceUri'])
+                appIdTag = existingData.properties.tags["appId"]
+
+                if 'appName' in existingData.properties.tags or 'owner' in existingData.properties.tags:
+                    print("Tag updates are already in place for resources")
+                    # Proper status code response prevent excessive retry
+                    return func.HttpResponse("Tag updates are already in place for resource", status_code=400)  
+    except:
+        print("Resource does not support tags: " + data['resourceUri'])
+
+        # If the request does not have an appId tag or does not support tags the function returns.
+        return func.HttpResponse("Resource does not support tags or does not contain AppId tag", status_code=400)
+
     # Query for complex tags from CosmosDB using the existing AppId tag
     for item in container.query_items(
         query=queryTemplate.substitute(n1 = appIdTag),
@@ -104,9 +154,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Copy query response into cosmosTagData dictionary
         cosmosTagData = item.copy()
 
-    # Upsert the resource tags for the given resourceUri using the queried tags
-    resource_client.tags.create_or_update_at_scope(data['resourceUri'], { "operation": "create", "properties": {
+    try:
+        # Create the resource tags for the given resourceUri using the queried tags from CosmosDB
+        resource_client.tags.create_or_update_at_scope(data['resourceUri'], { "operation": "create", "properties": {
         "tags": { "appId": appIdTag, "appName": cosmosTagData.get('appName'), "owner": cosmosTagData.get('owner'), "ctime": cosmosTagData.get('ctime') }
-    }})
+        }})
+    except:
+        print("Tag updates were unsuccessful for: " + data['resourceUri'])
+        # Proper status code response prevent excessive retry
+        return func.HttpResponse("Tag updates were unsuccessful for: " + data['resourceUri'], status_code=400)
 
-    return func.HttpResponse(f"Hello. This HTTP triggered function executed successfully.", 200)
+    print("Tag updates were successful for: " + data['resourceUri'])
+    # Proper status code response prevent excessive retry
+    return func.HttpResponse("Tag updates were successful for: " + data['resourceUri'], status_code=200)
